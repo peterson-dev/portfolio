339cfb0f4c5446c9292c8dd799a75df02e9a250c
Dan Dev <peterzen.dev@gmail.com>
2019-05-08 14:47:25 -0400

    add diagonal hero page v1

--- main/assets/js/util.js
+++ main/assets/js/util.js
@@ -1,6 +1,28 @@
 // Utility function
 function Util () {};
 
+
+/* 
+  menu link selector, home page, highlight class 
+*/
+function navHighlight(elem, home, active) {
+    var url = location.href.split('/'),
+        loc = url[url.length -1],
+        link = document.querySelectorAll(elem);
+    for (var i = 0; i < link.length; i++) {
+        var path = link[i].href.split('/'),
+            page = path[path.length -1];
+        if (page == loc || page == home && loc == '') {
+            link[i].parentNode.className += ' ' + active;
+            document.body.className += ' ' + page.substr(0, page.lastIndexOf('.'));
+            }
+        }
+    }
+navHighlight('.js-nav-hilight a', 'index.html', 'current'); 
+
+/*
+    main-header component function
+*/
 (function() {
 	var mainHeader = document.getElementsByClassName('js-main-header')[0];
 	if( mainHeader ) {
