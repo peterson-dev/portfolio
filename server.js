29556ba31ed59d4a60db2b165df077b39a53a01b
Dan Dev <peterzen.dev@gmail.com>
2019-05-11 17:38:32 -0400

    hf

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static('files'))
+app.use(express.static('main'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
