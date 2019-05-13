268f1dbe0798ceb065e7dcd8f0283bff304c40b4
Dan Dev <peterzen.dev@gmail.com>
2019-05-13 10:22:06 -0400

    move custom js script to it's own file, link script to each page

--- main/assets/js/util.js
+++ main/assets/js/util.js
@@ -1,102 +1,6 @@
 // Utility function
 function Util () {};
 
-
-/* 
-  menu link selector, home page, highlight class 
-*/
-function navHighlight(elem, home, active) {
-    var url = location.href.split('/'),
-        loc = url[url.length -1],
-        link = document.querySelectorAll(elem);
-    for (var i = 0; i < link.length; i++) {
-        var path = link[i].href.split('/'),
-            page = path[path.length -1];
-        if (page == loc || page == home && loc == '') {
-            link[i].parentNode.className += ' ' + active;
-            document.body.className += ' ' + page.substr(0, page.lastIndexOf('.'));
-            }
-        }
-    }
-navHighlight('.js-nav-hilight a', 'index.html', 'current'); 
-
-/*
-    main-header component function
-*/
-(function() {
-	var mainHeader = document.getElementsByClassName('js-main-header')[0];
-	if( mainHeader ) {
-		var trigger = mainHeader.getElementsByClassName('js-main-header__nav-trigger')[0],
-			nav = mainHeader.getElementsByClassName('js-main-header__nav')[0];
-		//detect click on nav trigger
-		trigger.addEventListener("click", function(event) {
-			event.preventDefault();
-			var ariaExpanded = !Util.hasClass(nav, 'main-header__nav--is-visible');
-			//show nav and update button aria value
-			Util.toggleClass(nav, 'main-header__nav--is-visible', ariaExpanded);
-			trigger.setAttribute('aria-expanded', ariaExpanded);
-			if(ariaExpanded) { //opening menu -> move focus to first element inside nav
-				nav.querySelectorAll('[href], input:not([disabled]), button:not([disabled])')[0].focus();
-			}
-		});
-	}
-}());
-
-/*
-    details component function
-*/
-
-(function() {
-	var Details = function(element, index) {
-		this.element = element;
-		this.summary = this.element.getElementsByClassName('js-details__summary')[0];
-		this.details = this.element.getElementsByClassName('js-details__content')[0];
-		this.htmlElSupported = 'open' in this.element;
-		this.initDetails(index);
-		this.initDetailsEvents();
-	};
-
-	Details.prototype.initDetails = function(index) {
-		// init aria attributes 
-		Util.setAttributes(this.summary, {'aria-expanded': 'false', 'aria-controls': 'details--'+index, 'role': 'button'});
-		Util.setAttributes(this.details, {'aria-hidden': 'true', 'id': 'details--'+index});
-	};
-
-	Details.prototype.initDetailsEvents = function() {
-		var self = this;
-		if( this.htmlElSupported ) { // browser supports the <details> element 
-			this.element.addEventListener('toggle', function(event){
-				var ariaValues = self.element.open ? ['true', 'false'] : ['false', 'true'];
-				// update aria attributes when details element status change (open/close)
-				self.updateAriaValues(ariaValues);
-			});
-		} else { //browser does not support <details>
-			this.summary.addEventListener('click', function(event){
-				event.preventDefault();
-				var isOpen = self.element.getAttribute('open'),
-					ariaValues = [];
-
-				isOpen ? self.element.removeAttribute('open') : self.element.setAttribute('open', 'true');
-				ariaValues = isOpen ? ['false', 'true'] : ['true', 'false'];
-				self.updateAriaValues(ariaValues);
-			});
-		}
-	};
-
-	Details.prototype.updateAriaValues = function(values) {
-		this.summary.setAttribute('aria-expanded', values[0]);
-		this.details.setAttribute('aria-hidden', values[1]);
-	};
-
-	//initialize the Details objects
-	var detailsEl = document.getElementsByClassName('js-details');
-	if( detailsEl.length > 0 ) {
-		for( var i = 0; i < detailsEl.length; i++) {
-			(function(i){new Details(detailsEl[i], i);})(i);
-		}
-	}
-}());
-
 /* 
 	class manipulation functions
 */
@@ -324,3 +228,25 @@ Math.easeInOutQuad = function (t, b, c, d) {
 	t--;
 	return -c/2 * (t*(t-2) - 1) + b;
 };
+
+/*
+    main-header component function
+*/
+(function() {
+	var mainHeader = document.getElementsByClassName('js-main-header')[0];
+	if( mainHeader ) {
+		var trigger = mainHeader.getElementsByClassName('js-main-header__nav-trigger')[0],
+			nav = mainHeader.getElementsByClassName('js-main-header__nav')[0];
+		//detect click on nav trigger
+		trigger.addEventListener("click", function(event) {
+			event.preventDefault();
+			var ariaExpanded = !Util.hasClass(nav, 'main-header__nav--is-visible');
+			//show nav and update button aria value
+			Util.toggleClass(nav, 'main-header__nav--is-visible', ariaExpanded);
+			trigger.setAttribute('aria-expanded', ariaExpanded);
+			if(ariaExpanded) { //opening menu -> move focus to first element inside nav
+				nav.querySelectorAll('[href], input:not([disabled]), button:not([disabled])')[0].focus();
+			}
+		});
+	}
+}());
